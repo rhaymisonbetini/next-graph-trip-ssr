fba2c4132381d625713bf683bdc4264e81b27b1e
RHAYMISON <rhaymison@hotmail.com>
2022-01-31 08:59:36 -0300

    create navigator icon

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,21 +1,43 @@
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
 
+type Places = {
+    id: string,
+    name: string,
+    slug: string,
+    location: {
+        latitude: number,
+        longitude: number
+    }
+}
 
-const Map = () => {
+export type MapProps = {
+    places: Places[]
+}
+
+
+const Map = ({ places }: MapProps) => {
     return (
         < MapContainer
             center={[0, 0]}
             zoom={3}
-            style={{ height:'100vh', width:'100%', position:'absolute' }} >
+            style={{ height: '100vh', width: '100%', position: 'absolute' }} >
             <TileLayer
                 attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
             />
-            <Marker position={[0, 0]}>
-                <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                </Popup>
-            </Marker>
+
+            {
+                places?.map(({ id, slug, name, location }) => {
+                    const { latitude, longitude } = location;
+                    return (
+                        <Marker position={[latitude, longitude]} title={name} key={`places-${id}`}>
+                            <Popup>
+                                A pretty CSS3 popup. <br /> Easily customizable.
+                            </Popup>
+                        </Marker>
+                    )
+                })
+            }
         </MapContainer >
     )
 }
